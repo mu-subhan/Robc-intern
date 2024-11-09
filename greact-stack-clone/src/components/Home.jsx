b69c657016932ce1db336f85d6e796e593c838b8
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-11-10 00:45:21 +0500

    add some styling but still have issue releated to responsivenes

--- greact-stack-clone/src/components/Home.jsx
+++ greact-stack-clone/src/components/Home.jsx
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/logo.png"
 import arrow from "../assets/arrow.png"
-
+import { FiMenu, FiX } from 'react-icons/fi' // We are using react-icons for the hamburger icon and close icon
 
 const Home = () => {
-  return (
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for toggling the menu
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
     <nav>
-      <div className='container'>
-        <img src={logo} alt='university-logo' className='logo' />
-      <ul className='items'>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <div className='contact'>
-        <li>
-          <a className='btn'>Contact us</a>
-        </li>
+      <div className="container">
+        <img src={logo} alt="university-logo" className="logo" />
+        
+        {/* Hamburger menu for mobile view */}
+        <div className="menu-toggle" onClick={toggleMenu}>
+          {isMenuOpen ? <FiX size={30} /> : <FiMenu size={30} />}
         </div>
-      </ul>
+
+        {/* Sidebar for mobile view */}
+        <div className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+          <ul className="items">
+            <li>Home</li>
+            <li>Program</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <div className="contact">
+              <li>
+                <a className="btn">Contact us</a>
+              </li>
+            </div>
+          </ul>
+        </div>
+
+        <ul className="items desktop-nav">
+          <li>Home</li>
+          <li>Program</li>
+          <li>About us</li>
+          <li>Campus</li>
+          <li>Testimonials</li>
+          <div className="contact">
+            <li>
+              <a className="btn">Contact us</a>
+            </li>
+          </div>
+        </ul>
       </div>
       <div className="hero-section">
-      <h1>We Ensure better education for a better world</h1>
-      <p>Our cutting-edge curriculum is designed to empower the students with the knowledge, skills and experience needed to excel in the dynamic field of education</p>
-      <div className="explore">
-        <p>Explore more</p>
-        <img src={arrow} alt="arrow" className="arrow" />
+        <h1>We Ensure better education for a better world</h1>
+        <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experience needed to excel in the dynamic field of education.</p>
+        <div className="explore">
+          <p>Explore more</p>
+          <img src={arrow} alt="arrow" className="arrow" />
+        </div>
       </div>
-    </div>
     </nav>
-    
- 
-    
   )
 }
 
